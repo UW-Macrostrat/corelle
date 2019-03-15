d22069f84bf62352ccb106cee339400d37694668
Daven Quinn <dev@davenquinn.com>
2019-03-15 15:24:42 -0500

    Basic rotation tests

--- corelle/rotate.py
+++ corelle/rotate.py
@@ -1,3 +1,5 @@
+import numpy as N
+import quaternion as Q
 from sqlalchemy import text
 from pg_viewtils import reflect_table
 from .database import db
@@ -5,12 +7,18 @@ from .database import db
 __plate = reflect_table(db, 'plate')
 __rotation = reflect_table(db, 'rotation')
 
+def euler_to_quaternion(euler_pole):
+    angles = N.radians(euler_pole)
+    return Q.from_euler_angles(angles)
+
 def get_rotation(plate_id, time):
     conn = db.connect()
 
     _ = "SELECT (rotation_sequence(:plate_id, :time)).*"
     res = conn.execute(text(_), plate_id=plate_id, time=time)
 
+    for plate in plates:
+        pass
 
     print(res.fetchall())
 
---
+++ corelle/test_rotate.py
@@ -0,0 +1,23 @@
+import numpy as N
+import quaternion
+from .util import vector, unit_vector
+
+equal = N.allclose
+
+def is_pure_quaternion(q):
+    """
+    A _pure quaternion_ is has a _real component_ (i.e.
+    the first component) of zero. This is a fancy way
+    of saying that it is _just a vector_.
+    """
+    return q.w == 0
+
+def test_quaternion_identity():
+    v1 = vector(0,0,1)
+    identity = N.quaternion(1,0,0,0)
+    # Numerical construction of quaternion
+    q = N.quaternion(0,*v1)
+    assert equal(q.vec, v1)
+    assert is_pure_quaternion(q)
+    v2 = q*identity
+    assert equal(v1,v2.vec)
