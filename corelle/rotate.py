7eeba9b74609503478e2d73259be1054f9cd635c
Daven Quinn <dev@davenquinn.com>
2019-08-01 10:37:40 -0400

    Partial more resilient version

--- corelle/rotate.py
+++ corelle/rotate.py
@@ -1,6 +1,6 @@
 import numpy as N
 import quaternion as Q
-from sqlalchemy import text
+from sqlalchemy import text, and_, desc
 from pg_viewtils import reflect_table
 from functools import lru_cache
 from time import sleep
@@ -9,6 +9,7 @@ from pg_viewtils import relative_path
 from .util import unit_vector
 from .database import db
 
+__model = reflect_table(db, 'model')
 __plate = reflect_table(db, 'plate')
 __rotation = reflect_table(db, 'rotation')
 
@@ -40,14 +41,81 @@ def quaternion_to_euler(q):
 
 conn = db.connect()
 
+class Stack(object):
+    def __init__(self):
+        self.ids = []
+        self.rotations = []
+        self.loops = []
+
+    def push(self, row, rotation):
+        if row.ref_plate_id in ids:
+            # There is a loop! we need to go back to before the loop
+            # Go back to the start of the loop
+            self.truncate_before(row.ref_plate_id)
+        self.ids.append(row.ref_plate_id)
+        self.rotations.append(rotation)
+
+    def truncate_before(self, id):
+        ix = self.ids.index(id)
+        self.loops.append(self.ids[ix])
+        self.ids = self.ids[:ix]
+        self.rotations = self.rotations[:ix]
+        self.items = self.items[:ix]
+
+    def next_id(self):
+        return self.ids[-1]
+
+
 # Cache this expensive, recursive function.
-@lru_cache(maxsize=50000)
-def get_rotation(model_name, plate_id, time, depth=0, verbose=False):
+@lru_cache(maxsize=5000)
+def __get_rotation(stack, model_query, plate_id, time, depth=0, verbose=False):
     time = float(time)
     # Fetches a sequence of rotations per unit time
-    fn = relative_path(__file__, 'query', 'rotation-sequence.sql')
-    sql = text(open(fn).read())
-    res = conn.execute(sql, model_name=model_name, plate_id=plate_id, time=time)
+
+    base_query = model_query.filter(__rotation.c.plate_id==plate_id)
+    _t = __rotation.c.t_step
+    rotations_before = base_query.filter(_t <= time).order_by(desc(_t))
+    rotations_after = base_query.filter(_t > time).order_by(_t)
+
+    prev_step = None
+    rotation = None
+    for r in rotations_before:
+        if stack.is_loop(r.ref_plate_id):
+            # We don't want to get into an endless loop
+            continue
+        q_before = euler_to_quaternion([
+            float(i) for i in [r.latitude,r.longitude,r.angle]
+        ])
+
+        base = __get_rotation(stack, model_query, stack.next_id(), time)
+        if r.t_step == time:
+            # The rotation is simply q_before
+
+        prev_step = float(r.t_step)
+        break
+
+    if not prev_step:
+        return None
+
+    for r in rotations_after:
+        if rotation is not None:
+            # We don't need to modify our rotation
+            break
+        if stack.is_loop(r.ref_plate_id):
+            continue
+        q_after = euler_to_quaternion([
+            float(i) for i in [r.latitude,r.longitude,r.angle]
+        ])
+        # Proportion of time between steps elapsed
+        proportion = (time-prev_step)/(float(r.t_step)-prev_step)
+        rotation = q_before*(1-proportion) + q_after*proportion
+
+
+    stack.push(rotation, q)
+    return __get_rotation(stack, model_query, stack.next_id(), time)
+
+
+
     rotations = res.fetchall()
     assert len(rotations) <= 2
 
