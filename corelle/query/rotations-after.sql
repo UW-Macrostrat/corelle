053cb725f69725aa0933a0cd26c6055e7e3f246d
Daven Quinn <dev@davenquinn.com>
2019-08-13 03:59:47 -0500

    Get rid of unused sql

--- corelle/query/rotations-after.sql
+++
@@ -1,7 +0,0 @@
-/* Get rotations at or before a time by plate ID */
-SELECT *
-FROM rotation
-WHERE t_step > :time
-  AND model_id = (SELECT id FROM model WHERE name = :model_name)
-  AND plate_id = :plate_id
-ORDER BY t_step
