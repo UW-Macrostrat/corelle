df77e6d40fc734a69bb90b2d762579ee9c868d4f
Daven Quinn <dev@davenquinn.com>
2019-08-22 16:23:58 +0200

    Remove functions etc that prevent us from starting from scratch without errors

--- corelle/cli.py
+++ corelle/cli.py
@@ -4,7 +4,6 @@ warnings.filterwarnings("ignore")
 import json, yaml
 import numpy as N
 from IPython import embed
-from .rotate.math import quaternion_to_euler
 
 from click import (
     group, argument, option,
@@ -78,6 +77,8 @@ def rotate(model, plate, time, verbose=False):
     Rotate a plate to a time
     """
     from .rotate import get_rotation
+    from .rotate.math import quaternion_to_euler
+
     q = get_rotation(model, plate, time, verbose=verbose)
     if q is None:
         echo(f"No rotation for plate {plate} at {time} Ma.")
