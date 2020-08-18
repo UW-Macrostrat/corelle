248db826e7224f9134b975a2c0c7803bc8556ffb
Daven Quinn <dev@davenquinn.com>
2020-08-18 16:49:02 -0500

    Format python files with Black

--- corelle/api.py
+++ corelle/api.py
@@ -6,72 +6,72 @@ from simplejson import loads, JSONEncoder
 
 from .database import db
 from .query import get_sql
-from .rotate import (
-    get_rotation,
-    get_all_rotations,
-    get_plate_rotations,
-    rotate_point
-)
+from .rotate import get_rotation, get_all_rotations, get_plate_rotations, rotate_point
 
 app = Flask(__name__)
-app.config['RESTFUL_JSON'] = dict(cls=JSONEncoder)
+app.config["RESTFUL_JSON"] = dict(cls=JSONEncoder)
 api = Api(app)
 
 conn = db.connect()
 
+
 class Help(Resource):
     def get(self):
-        return {'routes': []}
+        return {"routes": []}
+
 
 def get_plate_polygons(model):
     """
     Uncached version of modern plate polygons query
     """
-    q = get_sql('modern-plate-polygons')
+    q = get_sql("modern-plate-polygons")
     results = conn.execute(q, model_name=model)
     for row in results:
         props = dict(row)
         geom = loads(props.pop("geometry"))
         yield dict(
-            id = props['plate_id'],
-            type = 'Feature',
-            properties = props,
-            geometry = geom)
+            id=props["plate_id"], type="Feature", properties=props, geometry=geom
+        )
+
 
 def get_plate_polygons_cached(model):
-    q = get_sql('modern-plate-polygons-cached')
+    q = get_sql("modern-plate-polygons-cached")
     return conn.execute(q, model_name=model).scalar()
+
 
 class ModelResource(Resource):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.parser = RequestParser()
-        self.parser.add_argument('model', type=str, required=True)
+        self.parser.add_argument("model", type=str, required=True)
+
 
 class AllFeatures(Resource):
     def get(self):
         q = text("SELECT DISTINCT dataset_id FROM cache.feature")
         return [r[0] for r in conn.execute(q)]
 
+
 class Features(ModelResource):
     def get(self, dataset):
         args = self.parser.parse_args()
-        q = get_sql('feature-dataset-cached')
-        return conn.execute(q,
-            model_name=args['model'],
-            dataset=dataset).scalar()
+        q = get_sql("feature-dataset-cached")
+        return conn.execute(q, model_name=args["model"], dataset=dataset).scalar()
+
 
 class ModernPlates(ModelResource):
     def get(self):
         args = self.parser.parse_args()
-        return get_plate_polygons_cached(args['model'])
+        return get_plate_polygons_cached(args["model"])
+
 
 def base_parser():
     r = RequestParser()
-    r.add_argument('plate_id', type=int)
-    r.add_argument('model', type=str, required=True)
-    r.add_argument('quaternion', type=bool, default=False)
+    r.add_argument("plate_id", type=int)
+    r.add_argument("model", type=str, required=True)
+    r.add_argument("quaternion", type=bool, default=False)
     return r
+
 
 class RotationsResource(Resource):
     def __init__(self):
@@ -79,91 +79,94 @@ class RotationsResource(Resource):
         self.parser = base_parser()
 
     def reducer(self, q, args):
-        if args['quaternion']:
+        if args["quaternion"]:
             return dict(quaternion=[q.w, q.x, q.y, q.z])
-        return dict(
-            axis=list(q.vec),
-            angle=q.angle())
+        return dict(axis=list(q.vec), angle=q.angle())
+
 
 class Rotation(RotationsResource):
     def __init__(self):
         super().__init__()
-        self.parser.add_argument('time', required=True)
+        self.parser.add_argument("time", required=True)
 
     def reducer(self, q, args, plate_id):
         res = super().reducer(q, args)
-        res['plate_id'] = plate_id
+        res["plate_id"] = plate_id
         return res
 
     def get(self):
-        args =self.parser.parse_args()
+        args = self.parser.parse_args()
 
-        if args['plate_id']:
+        if args["plate_id"]:
             return self.get_single(args)
 
         return list(self.get_all(args))
 
     def get_all(self, args):
-        for (plate_id, q) in get_all_rotations(args['model'], args['time']):
+        for (plate_id, q) in get_all_rotations(args["model"], args["time"]):
             yield self.reducer(q, args, plate_id)
 
     def get_single(self, args):
-        plate_id = args['plate_id']
-        q = get_rotation(args['model'], plate_id, args['time'])
+        plate_id = args["plate_id"]
+        q = get_rotation(args["model"], plate_id, args["time"])
         return self.reducer(q, args, plate_id)
+
 
 class Pole(ModelResource):
     """
     Get poles for plate(s)
     """
+
     def __init__(self):
         super().__init__()
-        self.parser.add_argument('plate_id', type=int, required=True)
+        self.parser.add_argument("plate_id", type=int, required=True)
 
     def get(self):
         args = self.parser.parse_args()
-        id = args['plate_id']
+        id = args["plate_id"]
         return list(self.get_single(id, args))
 
     def get_single(self, id, args):
-        for q, t_step in get_plate_rotations(args['model'], id):
+        for q, t_step in get_plate_rotations(args["model"], id):
             yield self.reducer(q, args, t_step)
 
     def reducer(self, q, args, t_step):
         res = super().reducer(q, args)
-        res['t_step'] = t_step
+        res["t_step"] = t_step
         return res
+
 
 class Model(Resource):
     def get(self):
         results = conn.execute("SELECT id, name FROM model")
         return [dict(r) for r in results]
 
+
 class Point(ModelResource):
     def __init__(self):
         super().__init__()
-        self.parser.add_argument('data', type=str, required=True)
-        self.parser.add_argument('time', required=True)
-        self.parser.add_argument('include_failures', type=bool, default=False)
+        self.parser.add_argument("data", type=str, required=True)
+        self.parser.add_argument("time", required=True)
+        self.parser.add_argument("include_failures", type=bool, default=False)
+
     def get(self):
         args = self.parser.parse_args()
         points = args.data.split()
         out_points = []
         for p in points:
-            [lon,lat] = p.split(",")
+            [lon, lat] = p.split(",")
             pt = [float(lon), float(lat)]
-            res = rotate_point(pt, args['model'], args['time'])
+            res = rotate_point(pt, args["model"], args["time"])
             if res is not None:
-                res = dict(
-                    type="Feature",
-                    geometry=dict(type="Point", coordinates=res))
+                res = dict(type="Feature", geometry=dict(type="Point", coordinates=res))
                 out_points.append(res)
             else:
-                if args['include_failures']:
+                if args["include_failures"]:
                     out_points.append(None)
         return out_points
 
-api.add_resource(Help, '/api')
+
+api.add_resource(Help, "/api")
 api.add_resource(ModernPlates, "/api/plates")
 api.add_resource(Rotation, "/api/rotate")
 api.add_resource(AllFeatures, "/api/feature")
--- corelle/load_data.py
+++ corelle/load_data.py
@@ -11,38 +11,40 @@ from .database import db, create_session
 from .query import get_sql
 
 __conn = None
+
+
 def connect():
     global __conn
     if __conn is None:
         __conn = db.connect()
     return __conn
 
+
 def get_model(name):
-    model = reflect_table(db, 'model')
+    model = reflect_table(db, "model")
     conn = connect()
     try:
         conn.execute(model.insert().values(name=name))
     except IntegrityError:
         pass
-    return conn.execute(
-        model.select().where(model.c.name==name)
-    ).first()[0]
+    return conn.execute(model.select().where(model.c.name == name)).first()[0]
 
-__plate = reflect_table(db,'plate')
-__feature = reflect_table(db, 'feature')
-__rotation = reflect_table(db,'rotation')
-__plate_polygon = reflect_table(db, 'plate_polygon')
+
+__plate = reflect_table(db, "plate")
+__feature = reflect_table(db, "feature")
+__rotation = reflect_table(db, "rotation")
+__plate_polygon = reflect_table(db, "plate_polygon")
+
 
 def pg_geometry(feature):
-    geom = dumps(feature['geometry'])
+    geom = dumps(feature["geometry"])
     _ = func.ST_GeomFromGeoJSON(geom)
     return func.ST_SetSRID(func.ST_MakeValid(func.ST_Multi(_)), 4326)
 
+
 def insert_plate(**vals):
-    connect().execute(
-        insert(__plate)
-            .values(vals)
-            .on_conflict_do_nothing())
+    connect().execute(insert(__plate).values(vals).on_conflict_do_nothing())
+
 
 def import_plate(model_id, feature, fields=None):
 
@@ -53,19 +55,20 @@ def import_plate(model_id, feature, fields=None):
         # geojson file, we should use these fields for
         if fields and field_id in fields:
             field_id = fields[field_id]
-        return feature['properties'].get(field_id, None)
+        return feature["properties"].get(field_id, None)
 
     plate_id = field("id")
 
     insert_plate(
         id=plate_id,
         model_id=model_id,
-        parent_id=field('parent_id'),
-        name=field('name'),
-        cotid=field('cotid'),
-        coid=field('coid'))
+        parent_id=field("parent_id"),
+        name=field("name"),
+        cotid=field("cotid"),
+        coid=field("coid"),
+    )
 
-    young_lim = field('young_lim')
+    young_lim = field("young_lim")
     if young_lim == -999:
         young_lim = None
 
@@ -73,16 +76,16 @@ def import_plate(model_id, feature, fields=None):
         plate_id=plate_id,
         model_id=model_id,
         young_lim=young_lim,
-        old_lim=field('old_lim'),
-        geometry=pg_geometry(feature))
+        old_lim=field("old_lim"),
+        geometry=pg_geometry(feature),
+    )
 
-    conn.execute(__plate_polygon
-            .insert()
-            .values(poly_vals))
+    conn.execute(__plate_polygon.insert().values(poly_vals))
+
 
 def import_plates(model_id, plates, fields={}):
     session = create_session()
-    with fiona.open(plates, 'r') as src:
+    with fiona.open(plates, "r") as src:
         conn = connect()
         trans = conn.begin()
 
@@ -90,17 +93,11 @@ def import_plates(model_id, plates, fields={}):
         if fields:
             mrf = fields.get("mantle_reference_frame", None)
         if mrf is not None:
-            insert_plate(
-                id=mrf,
-                model_id=model_id,
-                name='Mantle reference frame')
+            insert_plate(id=mrf, model_id=model_id, name="Mantle reference frame")
 
         # The spin axis is assumed to have plate ID 0 by default
         spin_axis = fields.get("spin_axis", 0)
-        insert_plate(
-            id=spin_axis,
-            model_id=model_id,
-            name='Spin axis')
+        insert_plate(id=spin_axis, model_id=model_id, name="Spin axis")
 
         for feature in src:
             import_plate(model_id, feature, fields=fields)
@@ -110,51 +107,58 @@ def import_plates(model_id, plates, fields={}):
         # For faster updates, this materialized view could become an actual table
         conn.execute("REFRESH MATERIALIZED VIEW cache.plate_polygon")
 
+
 def import_feature(dataset, feature):
     conn = connect()
 
     vals = dict(
         dataset_id=dataset,
-        properties=(feature['properties'] or None),
-        geometry=pg_geometry(feature))
+        properties=(feature["properties"] or None),
+        geometry=pg_geometry(feature),
+    )
 
     conn.execute(__feature.insert().values(vals))
+
 
 def import_features(name, features, overwrite=False):
     session = create_session()
     start = perf_counter()
     conn = connect()
-    with fiona.open(features, 'r') as src:
+    with fiona.open(features, "r") as src:
         trans = conn.begin()
 
         if overwrite:
-            conn.execute(__feature
-                .delete()
-                .where(__feature.c.dataset_id == name))
+            conn.execute(__feature.delete().where(__feature.c.dataset_id == name))
 
         for i, feature in enumerate(src):
             import_feature(name, feature)
         trans.commit()
     step1 = perf_counter()
-    elapsed = step1-start
-    echo(f"Imported {i+1} features for dataset "+style(name, bold=True)+f" in {elapsed:.2f} seconds")
+    elapsed = step1 - start
+    echo(
+        f"Imported {i+1} features for dataset "
+        + style(name, bold=True)
+        + f" in {elapsed:.2f} seconds"
+    )
 
-    sql = get_sql('cache-feature-dataset')
+    sql = get_sql("cache-feature-dataset")
     trans = conn.begin()
     conn.execute(sql, dataset_id=name)
     trans.commit()
 
-    elapsed = perf_counter()-step1
+    elapsed = perf_counter() - step1
     echo(f"  cached transformed features in {elapsed:.2f} seconds")
+
 
 def import_rotation_row(model_id, line):
     """
     Import a line from a GPlates rotations file
     """
-    if line.startswith('*'): return
+    if line.startswith("*"):
+        return
 
     conn = connect()
-    data, meta = line.strip().split("!",1)
+    data, meta = line.strip().split("!", 1)
     row = data.split()
 
     if int(row[0]) == 999:
@@ -166,13 +170,8 @@ def import_rotation_row(model_id, line):
     # Not sure that every plate_id-ref_plate_id
     # pair is actually unique as would be needed
     # here...
-    insert_plate(
-        id=ref_plate_id,
-        model_id=model_id)
-    insert_plate(
-        id=plate_id,
-        model_id=model_id,
-        parent_id=ref_plate_id)
+    insert_plate(id=ref_plate_id, model_id=model_id)
+    insert_plate(id=plate_id, model_id=model_id, parent_id=ref_plate_id)
 
     vals = dict(
         plate_id=plate_id,
@@ -183,9 +182,11 @@ def import_rotation_row(model_id, line):
         angle=row[4],
         ref_plate_id=ref_plate_id,
         # Postgres chokes on NULL characters in strings
-        metadata=meta.replace('\x00', ''))
+        metadata=meta.replace("\x00", ""),
+    )
     _ = __rotation.insert().values(vals)
     conn.execute(_)
+
 
 def import_rotations(model_id, rotations):
     """
@@ -198,19 +199,21 @@ def import_rotations(model_id, rotations):
     form.
     """
     conn = connect()
-    with open(rotations, 'r') as f:
+    with open(rotations, "r") as f:
         start = perf_counter()
         for i, line in enumerate(f):
             import_rotation_row(model_id, line)
-        elapsed = perf_counter()-start
+        elapsed = perf_counter() - start
         print(f"Imported {i+1} rotations in {elapsed:.2f} seconds")
+
 
 def cache_rotations(model_id):
     return
-    v = db.execute(__rotation.select(__rotation.c.model_id==model_id))
+    v = db.execute(__rotation.select(__rotation.c.model_id == model_id))
     for row in v:
         q = get_rotation(row.plate_id)
         print(row.plate)
+
 
 def import_model(name, plates, rotations, fields=None, overwrite=False):
     conn = connect()
