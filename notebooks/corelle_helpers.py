08bcfecca10a4447221c1c54a6e69c0ef749cdb0
Daven Quinn <dev@davenquinn.com>
2020-09-18 11:11:36 -0500

    Added iPython notebook

--- notebooks/corelle_helpers.py
+++ notebooks/corelle_helpers.py
@@ -4,21 +4,24 @@ library, for rotating plates provided by the Corelle API.
 """
 import sys
 import os
+import numpy as N
 import quaternion as Q
 from shapely.ops import transform
 from shapely.geometry import shape
+from collections.abc import Iterable
 
 # Add corelle to our path
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 from corelle.rotate.math import cart2sph, sph2cart
-from collections.abc import Iterable
 
 
 def rotate_point(q, point):
     if isinstance(point[0], Iterable):
         raise TypeError("Function is not vectorized")
+        return point
     v0 = sph2cart(*point)
+    q = N.quaternion(*q)
     v1 = Q.rotate_vectors(q, v0)
     return cart2sph(v1)
 
@@ -40,5 +43,6 @@ def rotate_features(
 
     for f in features:
         q = rot_index.get(plate_id(f))
-        print(q)
+        if q is None:
+            continue
         yield rotate_geometry(q, shape(f["geometry"]))
