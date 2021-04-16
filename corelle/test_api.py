e131f589dadd273056800cbc2c48ed6cc5ef4ade
Daven Quinn <dev@davenquinn.com>
2021-04-16 03:53:50 -0500

    Added a test and new dependency

--- corelle/test_api.py
+++ corelle/test_api.py
@@ -1,6 +1,7 @@
 from pytest import fixture
 import sys
 from os import path
+from numpy import allclose
 
 # Get the right directory for corelle_helpers
 pth = path.abspath(path.join(path.dirname(__file__), "..", "notebooks"))
@@ -23,3 +24,19 @@ def test_basic_api_access(client):
     # Rotate our coastline featrues
     rotated_features = list(rotate_features(rotations, features))
     assert len(rotated_features) > 10
+
+def test_reconstruct_api(client):
+    args = dict(
+        lng=-89.37088151960317,
+        lat=43.084235832279795,
+        age=500,
+        model="Scotese",
+        referrer="rockd"
+    )
+    vals = "&".join([f"{k}={v}" for k,v in args.items()])
+    res = client.get("/api/reconstruct?"+vals).json
+
+    assert res["type"] == "FeatureCollection"
+    assert len(res["features"]) == 1 
+    coords = res["features"][0]["geometry"]["coordinates"]
+    assert allclose(coords, [-84.17949814030625, -21.341188167880905])
