900f060fa7b81b097bb5da3e11295b00e6dd8736
Daven Quinn <dev@davenquinn.com>
2022-11-30 01:00:46 -0500

    Refactor to break up files

---
+++ py-packages/engine/corelle/tests/rotation_testing_functions.py
@@ -0,0 +1,186 @@
+"""
+Tests of rotations using PostGIS functions (to support on-database transformations with cached rotations)
+"""
+
+from corelle.math.util import unit_vector
+import numpy as N
+import quaternion as Q
+from corelle.engine.database import db
+from geoalchemy2.shape import to_shape
+from geoalchemy2.elements import WKBElement
+from corelle.math import sph2cart, cart2sph
+
+
+def rotate_point(point, quaternion, func="corelle.rotate_geometry"):
+    # Rotate a point using the PostGIS function
+    sql = f"SELECT {func}(ST_SetSRID(ST_MakePoint(:lon, :lat), 4326), :quaternion)"
+    # Get the result of the rotation as a WKBElement
+    result = db.session.execute(
+        sql,
+        params=dict(
+            lon=point[0],
+            lat=point[1],
+            quaternion=[quaternion.w, quaternion.x, quaternion.y, quaternion.z],
+        ),
+    ).scalar()
+    assert result is not None
+    # Convert the WKBElement to a Shapely geometry
+    geom = to_shape(WKBElement(result))
+    return list(geom.coords)
+
+
+def rotate_with_ob_tran(start_pos, lon_p, lat_p, lon_0):
+    proj_string = f"+proj=ob_tran +o_proj=longlat +o_lon_p={lon_p}r +o_lat_p={lat_p}r +lon_0={lon_0}r"
+    sql = "SELECT ST_Transform(ST_SetSRID(ST_MakePoint(:x, :y), 4326), :proj_string)"
+    # Get the result of the rotation as a WKBElement
+    with db.session_scope() as session:
+        result = session.execute(
+            sql,
+            params=dict(
+                x=start_pos[0],
+                y=start_pos[1],
+                proj_string=proj_string,
+            ),
+        ).scalar()
+    # Convert the WKBElement to a Shapely geometry
+    geom = to_shape(WKBElement(result))
+    return list(geom.coords)
+
+
+def rotate_postgis_ob_tran(point, q):
+    # Step 1: Take the pole and rotate it along a meridian
+    # to a new location (pole is at some point on the equator)
+
+    # Check that we can recover the angle including sign from the quaternion
+    # angle = 2 * N.arccos(q.w)
+    # assert N.allclose(angle, N.radians(case.angle))
+
+    # Find new pole location
+    orig_pole = sph2cart(0, 90)
+    new_pole = Q.rotate_vectors(q, orig_pole)
+    new_pole_sph = cart2sph(new_pole)
+
+    pole_rotation_angle = N.degrees(N.arccos(N.dot(orig_pole, new_pole)))
+
+    print("Pole rotation angle:", pole_rotation_angle)
+    # Sanity check: the angle between the original pole and new pole
+    # should be less than or equal to the overall rotation angle
+    assert (
+        pole_rotation_angle <= N.degrees(N.abs(q.angle())) + 1e-6
+    )  # Allow for floating point error
+
+    # Sanity check 2: pole was rotated along a meridian
+    assert N.allclose(90 - new_pole_sph[1], pole_rotation_angle)
+
+    # Decompose the quaternion rotation into two rotations (swing-twist decomposition)
+    # 1. Rotation around the geographic pole
+    # 2. Rotation to move pole to a new location
+
+    swing_axis = unit_vector(*N.cross(orig_pole, new_pole))
+    swing_angle = N.arccos(N.dot(orig_pole, new_pole))
+
+    swing_q = Q.from_rotation_vector(swing_angle * swing_axis)
+
+    # Check that the rotation axis for the "swing" is indeed on the equator
+    assert N.allclose(swing_q.z, 0)
+    # Check that the swing angle is equivalent to the latitude (from north) of the new pole
+    assert N.allclose(N.degrees(swing_angle), 90 - cart2sph(new_pole)[1])
+
+    # Get rotation component around new pole (the "twist")
+    twist_q = q * swing_q.inverse()
+
+    # Check that two rotations can be composed back to the original
+    assert N.allclose(q, swing_q * twist_q)
+
+    # Step 2: Rotate around the new pole to a final angular position
+
+    # Apply the twist rotation
+    twisted = Q.rotate_vectors(twist_q, unit_vector(1, 0, 0))
+    twist_angle = N.degrees(N.arctan2(twisted[1], twisted[0]))
+
+    print("New pole:", new_pole_sph)
+
+    # For some reason, changing the longitude of the north pole causes the entire manifold to be shifted
+    # by that amount. I think this is a PROJ quirk? So we need to subtract this to rotate everything back into alignment.
+    lon_0 = new_pole_sph[0] - twist_angle
+    print("Twist angle:", twist_angle)
+    print("Lon_0:", lon_0)
+
+    return rotate_with_ob_tran(
+        point,
+        N.radians(new_pole_sph[0]),
+        N.radians(new_pole_sph[1]),
+        N.radians(lon_0),
+    )
+
+
+def rotate_postgis_simplified(point, q):
+    # Step 1: Take the pole and rotate it along a meridian
+    # to a new location (pole is at some point on the equator)
+
+    # Find new pole location
+    # new_pole = q * N.quaternion(0, 0, 0, 1) * q.inverse()
+    q1 = N.quaternion(
+        -q.z,
+        q.y,
+        -q.x,
+        q.w,
+    )
+    new_pole = q1 * q.inverse()
+
+    lon_p = N.arctan2(new_pole.y, new_pole.x)
+    lat_p = N.arcsin(new_pole.z)
+
+    # Pole rotation angle
+    lon_s = lon_p + N.pi / 2
+
+    half_angle = N.arccos(new_pole.z) / 2
+
+    swing_q_inv = N.quaternion(
+        -N.cos(half_angle),
+        N.cos(lon_s) * N.sin(half_angle),
+        N.sin(lon_s) * N.sin(half_angle),
+        0,
+    )
+
+    # Get rotation component around new pole (the "twist")
+    twist_q = q * swing_q_inv
+
+    # Step 2: Rotate around the new pole to a final angular position
+
+    twisted = twist_q * N.quaternion(0, 1, 0, 0) * twist_q.inverse()
+    twist_angle = N.arctan2(twisted.y, twisted.x)
+
+    # For some reason, changing the longitude of the north pole causes the entire manifold to be shifted
+    # by that amount. I think this is a PROJ quirk? So we need to subtract this to rotate everything back into alignment.
+    lon_0 = lon_p - twist_angle
+
+    return rotate_with_ob_tran(point, lon_p, lat_p, lon_0)
+
+
+def rotate_python(point, quaternion):
+    pt = sph2cart(*point)
+    v1 = Q.rotate_vectors(quaternion, pt)
+    return cart2sph(unit_vector(*v1))
+
+
+def corelle_rotate_geometry(point, quaternion):
+    return rotate_point(point, quaternion, func="corelle.rotate_geometry")
+
+
+def corelle_rotate_geometry_pointwise(point, quaternion):
+    return rotate_point(point, quaternion, func="corelle.rotate_geometry_pointwise")
+
+
+postgis_rotation_functions = [
+    "corelle.rotate_geometry",
+    "corelle.rotate_geometry_pointwise",
+]
+
+rotation_functions = [
+    corelle_rotate_geometry,
+    corelle_rotate_geometry_pointwise,
+    rotate_postgis_ob_tran,
+    rotate_postgis_simplified,
+    rotate_python,
+]
