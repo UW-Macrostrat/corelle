bb507ea092a6e02e42452a6b49512b20896b34a8
Daven Quinn <dev@davenquinn.com>
2023-11-08 02:40:59 -0600

    Attempt to make it quicker to load data

--- py-packages/engine/corelle/engine/cache.py
+++ py-packages/engine/corelle/engine/cache.py
@@ -16,6 +16,7 @@ from .query import get_sql
 
 update_derived = get_sql("update-cache")
 
+
 def get_from_cache(cache_args):
     # Get a rotation from the database cache
     (model_name, plate_id, time) = cache_args
@@ -52,41 +53,47 @@ def build_rotation_cache(model, time_step=1):
     # Get model steps every 1 Myr
     t_steps = list(range(int(min_age), int(max_age) + 1, time_step))
 
-    with Progress() as progress:
-        task = progress.add_task(
-            f"Building cache for model {model.name}", total=len(t_steps)
-        )
-        rotations = get_rotation_series(model.name, *t_steps)
+    session = db.session()
 
-        for tstep in rotations:
-            time = tstep["time"]
-            rows = [
-                build_cache_row(model.id, plate_id, time, q)
-                for plate_id, q in tstep["rotations"]
-                if q is not None
-            ]
-            add_to_cache(rows)
+    session.execute(
+        "DELETE FROM corelle.rotation_cache WHERE model_id = :model_id",
+        dict(model_id=model.id),
+    )
+    session.commit()
 
-            # Add derived columns
-            #conn.execute(update_derived, model_id=model.id, t_step=time)
-            progress.advance(task)
+    rotations = get_rotation_series(model.name, *t_steps)
+
+    for tstep in rotations:
+        time = tstep["time"]
+        print(model.name, " - ", time, " Ma")
+        rows = [
+            build_cache_row(model.id, plate_id, time, q)
+            for plate_id, q in tstep["rotations"]
+            if q is not None
+        ]
+        add_to_cache(session, rows)
+
+        # Add derived columns
+        # session.execute(update_derived, model_id=model.id, t_step=time)
+        # progress.advance(task)
 
 
 def build_cache_row(model_id, plate_id, t_step, q):
-    lat, lon, angle = quaternion_to_euler(q)
-    # Create geometry as WKT
-    geom = f"SRID=4326;POINT({lon} {lat})"
+    # lat, lon, angle = quaternion_to_euler(q)
+    # # Create geometry as WKT
+    # geom = f"SRID=4326;POINT({lon} {lat})"
 
     return dict(
         model_id=int(model_id),
         plate_id=int(plate_id),
         t_step=float(t_step),
-        rotation=[q.w, q.x, q.y, q.z]
+        rotation=[q.w, q.x, q.y, q.z],
     )
 
 
-def add_to_cache(data):
+def add_to_cache(session, data):
     if not data:
         return
     # Add a rotation to the database cache
-    conn.execute(insert(_rotation_cache).on_conflict_do_nothing(), data)
+    session.execute(insert(_rotation_cache), data)
+    session.commit()
