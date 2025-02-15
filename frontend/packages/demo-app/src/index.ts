c0c12ed74aa8f3a5c3cb88c3ef0785e3967d74ca
Daven Quinn <code@davenquinn.com>
2025-02-14 21:31:46 -0600

    Compiles with vite

--- frontend/packages/demo-app/src/index.ts
+++ frontend/packages/demo-app/src/index.ts
@@ -4,13 +4,15 @@ import { FocusStyleManager } from "@blueprintjs/core";
 import "@blueprintjs/core/lib/css/blueprint.css";
 
 FocusStyleManager.onlyShowFocusOnTabs();
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import h from "@macrostrat/hyper";
 import App from "./app";
 
-let baseURL =
-  process.env.CORELLE_API_BASE_URL || "https://rotate.macrostrat.org/api";
+let baseURL = import.meta.env.VITE_CORELLE_API_BASE_URL ?? "https://rotate.macrostrat.org/api";
 
 const div = document.createElement("div");
 document.body.appendChild(div);
-render(h(App, { baseURL }), div);
+
+const root = createRoot(div);
+
+root.render(h(App, { baseURL }));
--- frontend/packages/demo-app/vite.config.ts
+++ frontend/packages/demo-app/vite.config.ts
@@ -1,7 +1,22 @@
 import {defineConfig} from "vite";
-
+// import * as path from "node:path";
+// import * as fs from "node:fs";
+//
+// // Walk up the directory tree until we find an `.env` file
+// const maxParents = 3;
+// let envDir = __dirname;
+// for (let i = 0; i < maxParents; i++) {
+//   const envPath = path.join(envDir, ".env");
+//   if (fs.existsSync(envPath)) {
+//     envDir = envPath;
+//     break;
+//   }
+//   envDir = path.resolve(path.join(envDir, ".."));
+// }
+//
 export default defineConfig({
   resolve: {
     conditions: ["source"],
-  }
+  },
+  //envDir,
 });
